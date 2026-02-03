b53b1c29b85250f1aa9731ad245961867d37f40d
EdwardElricFA <lostinaddiction@gmail.com>
2026-02-03 19:42:51 +0800

    STEP: Basic Calendar View

--- 2026/InfiniteScrollViewCalendar/InfiniteScrollViewCalendar/View/CalendarView.swift
+++ 2026/InfiniteScrollViewCalendar/InfiniteScrollViewCalendar/View/CalendarView.swift
@@ -17,12 +17,35 @@ struct CalendarView: View {
             LazyVStack(spacing: 0) {
                 ForEach(months) { month in
                     MonthView(month: month)
+                        .frame(height: monthHeight)
                 }
             }
         }
         .scrollPosition($scrollPosition)
         .defaultScrollAnchor(.center)
         .onAppear(perform: loadInitialDate)
+        .safeAreaInset(edge: .top, spacing: 0) {
+            SymbolView()
+        }
+    }
+    
+    @ViewBuilder
+    func SymbolView() -> some View {
+        HStack(spacing: 0) {
+            ForEach(Calendar.current.shortWeekdaySymbols, id: \.self) { symbol in
+                Text(symbol)
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.secondary)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 15)
+        .overlay(alignment: .bottom) {
+            Divider()
+        }
+        .background(.ultraThinMaterial)
     }
     
     func loadInitialDate() {
@@ -38,7 +61,56 @@ struct CalendarView: View {
 struct MonthView: View {
     var month: Month
     var body: some View {
-        Text("Hello world")
+        VStack(alignment: .leading, spacing: 0) {
+            Text(month.name)
+                .font(.title2)
+                .fontWeight(.bold)
+                .frame(height: 50, alignment: .bottom)
+            
+            /// Weeks View
+            VStack(spacing: 0) {
+                ForEach(month.weeks) { week in
+                    HStack(spacing: 0) {
+                        /// Days View
+                        ForEach(week.days) { day in
+                            DaysView(day: day)
+                        }
+                    }
+                    .frame(maxHeight: .infinity)
+                    .overlay(alignment: .bottom) {
+                        if !week.isLast {
+                            Divider()
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 15)
+    }
+}
+
+
+/// Days View
+struct DaysView: View {
+    var day: Day
+    var body: some View {
+        if let dayValue = day.value, let date = day.date, !day.isPlaceholder {
+            let isToday = Calendar.current.isDateInToday(date)
+            
+            Text("\(dayValue)")
+                .font(.callout)
+                .fontWeight(isToday ? .semibold : .regular)
+                .foregroundStyle(isToday ? .white : .primary)
+                .frame(width: 30, height: 50)
+                .background {
+                    if isToday {
+                        Circle().fill(.blue.gradient)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+        } else {
+            Color.clear
+        }
     }
 }
 
